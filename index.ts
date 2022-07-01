9e4a308ca153ba87f7a83e79fe83738369642aa8
Eamon Sheil <Eamondsheil@gmail.com>
2022-06-30 23:52:59 -0400

    photo download

--- index.ts
+++ index.ts
@@ -29,13 +29,51 @@ export class CapitalCityScraper {
         const areaRows = $('.mergedtoprow th:contains(Area)').parent().nextUntil('.mergedtoprow');
             // then, get the descendant of areaRows with a th containing the text "Capital city", 
             // and a child 'td' element 
-        const area = areaRows.find(`th:contains(Capital city) + td`).text().trim();
-        console.log(area)
+            // also remove unwanted text with .replace() method
+        const area:string = areaRows.find(`th:contains(Capital city) + td`).text().trim().replace(/km2.*$/, '');
+            // remove commas from area, convert to number
+        const areanum:number = parseFloat(area.replace(/,/g,''));
+        console.log(areanum)
 
         const popRows = $('.mergedtoprow th:contains(Population)').parent().nextUntil('.mergedtoprow');
-        const pop = popRows.find('.mergedtoprow th:contains(Capital city) + td').text().trim();
-        console.log("---",)
+        const pop:string = popRows.find('th:contains(Capital city) + td').text().trim();
+            // replaces any text after the number with an empty string
+        const population:number = parseFloat(pop.replace(/,/g,''));
+        console.log("---", population);
+
+            // selecting the div containg 'Flag' that is next to the <a> element with a class of 'image'
+            // .prev() then gets the previous sibling of that
+            // .attr() is used to get the value of that element's href attribute
+            //  NOTE .attr() canbe used to dynamically set or change an href attribute, or get the value of any attribute
+        const flagPageLink:string = $('.mergedtoprow a.image + div:contains(Flag)').prev().attr('href')!;
+        const flagPageUrl:string = new URL(flagPageLink, url).toString();
+        const flagImagePath = await this.scrapeImage(flagPageUrl)
+        console.log(flagPageUrl)
     }
+    protected async scrapeImage(url:string){
+        const response = await axios.get(url);
+        const html = response.data;
+
+        const $ = cheerio.load(html);
+
+        const imageLink = $('#file a').attr('href')!;
+        const imageUrl = new URL(imageLink, url).toString();
+
+        const imagePath = await this.downloadFile(imageUrl, 'flags');
+        return imagePath;
+    }
+
+    protected async downloadFile(url:string, dir:string) {
+        const response = await axios.get(url, { 
+            responseType: 'arraybuffer'
+        });
+
+        fs.mkdirSync(dir, {recursive:true});
+
+        const filePath = path.join(dir, path.basename(url));
+        fs.writeFileSync(filePath,response.data)
+    }
+
 }
 
 async function main(city:string, obj?:object):Promise<void> {
